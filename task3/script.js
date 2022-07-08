0f0ad2281f37b31fe1958a3d9b3ea40dc7144d7b
Vysakh <vysakh1801@gmail.com>
2022-07-08 06:40:07 +0530

    JS Assignment commit 1

--- task3/script.js
+++ task3/script.js
@@ -1,1 +1,18 @@
-console.log("Hi")
+function emailValidate() {
+    var email = document.Ecreate.email;
+    var mailformat = "";
+    if(email.value.match(mailformat))
+    {
+    return true;
+    }
+    else
+    {
+    alert("You have entered an invalid email address!");
+    email.focus();
+    return false;
+    }
+}
+
+function fSubmit() {
+    emailValidate();
+}
