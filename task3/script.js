ba338dd941fb931faff547dcaf246e8b4fc204b0
Vysakh <vysakh1801@gmail.com>
2022-07-08 11:46:02 +0530

    HTML/CSS Assignment commit 6

--- task3/script.js
+++ task3/script.js
@@ -1,18 +1,44 @@
+var form = document.getElementById("Ecreate");
+form.addEventListener('submit', fSubmit);
+
 function emailValidate() {
-    var email = document.Ecreate.email;
-    var mailformat = "";
-    if(email.value.match(mailformat))
-    {
+    // var email = document.Ecreate.email;
+    // var mailformat = "";
+    // if(email.value.match(mailformat))
+    // {
+    // return true;
+    // }
+    // else
+    // {
+    // alert("You have entered an invalid email address!");
+    // email.focus();
+    // return false;
+    // }
     return true;
-    }
-    else
-    {
-    alert("You have entered an invalid email address!");
-    email.focus();
-    return false;
-    }
 }
 
-function fSubmit() {
-    emailValidate();
+function fValidate(){
+
+}
+
+function fConsolidate(){
+    var inObj = {
+        name : document.getElementById("ename").value,
+    }
+    return inObj;
+}
+
+function debounce(){
+
+}
+
+function fSubmit(event) {
+    event.preventDefault();
+    // debounce();
+    sbtn = document.getElementById("create")
+    if(emailValidate()){
+        var a = fConsolidate();
+    }
+    sbtn.classList.add("disabled");
+    console.log(a);
 }
